d8ebdfb26b9b39b85469a4d4319d1b1150641e02
Vincent Yang <yf2005@gmail.com>
2023-05-26 21:14:51 +0800

    add investbanking, async on fastapi

--- gptfuc.py
+++ gptfuc.py
@@ -514,6 +514,8 @@ def industry_name_to_code(industry_name):
         return "fund"
     elif industry_name == "期货":
         return "futures"
+    elif industry_name == "投行":
+        return "invbank"
     else:
         return "other"
 
